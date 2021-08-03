3ed76400e721cd7aa2174f8c93413287d2c12de9
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-08-03 19:19:58 +0200

    functionality to publish new comment

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -8,6 +8,35 @@ const Comment = require("../models/Comment.model");
 // POST route - create a comment of a specific post
 // ****************************************************************************************
 
-// ... your code here
+router.post('/posts/:postId/comment', (req, res, next) => {
+  const { postId } = req.params;
+  const { author, content } = req.body;
+
+  User.find( {username: author} )
+    .then( userFromDB => {
+      if( userFromDB.length) {
+        // Create comment
+        return Comment.create( {author: userFromDB._id, content});
+      } else {
+        res.status(403).send("user does not exist");
+        return Promise.reject("user does not exist");
+      }
+    })
+    .then( commentCreated => {
+      return Post.findByIdAndUpdate( 
+        postId, 
+        { $push: { comments: commentCreated._id  } }, 
+        { new: true }
+      );
+    })
+    .then( postFromDB => {
+      res.render('posts/details', postFromDB);
+    })
+    .catch(err => {
+      console.log(`Err publishing comment: ${err}`);
+      next(err);
+    });
+
+});
 
 module.exports = router;
